cfa2bddfad76f90603e7ba8e8a51afd57df3b93e
hifilorau <tomrau@gmail.com>
2015-06-19 22:34:26 -0400

    lots

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,3 +1,59 @@
   app.slider = function () {
-        $(".about-content-slider").lightSlider();
+        var heroTxt = $('#rau-holder');
+				var aboutContent=$('.about-content');
+				var aboutButton = $('.about-button');
+		    var workButton =$('.work-button');
+		
+			
+		  $('.flexslider').flexslider({
+					controlNav: false,
+				  slideshowSpeed: 40000, 
+				  prevText: "i",
+				  nextText: "h"
+				  
+				});
+		 
+		var sliderCtrl =$('.flex-direction-nav');
+		  sliderCtrl.hide();
+		  aboutContent.hide();
+		  $('.work').hide();
+		  
+			
+		
+//			aboutButton.click(function(){
+//			heroTxt.hide();
+//			$('.work').hide();
+//			aboutContent.show();
+//			});
+		
+			$('.home').click(function(){
+				sliderCtrl.hide();
+				aboutContent.hide();
+				$('.work').hide();
+				heroTxt.show();
+//				app.transformTxt();
+				console.log('fire');
+				
+				
+			});
+		
+		
+		   workButton.click(function(){
+				heroTxt.hide();
+				aboutContent.hide();
+				$('.work').show();				
+				sliderCtrl.show();	
+			});
+		
+		 aboutButton.click(function () {
+			 	heroTxt.hide();
+			$('.work').hide();
+			aboutContent.show();
+//          $('.about-content').show(".about-content", { direction: "left" }, 1000);
+    });
+		   
+			 
+  
+		
+		    
     };
